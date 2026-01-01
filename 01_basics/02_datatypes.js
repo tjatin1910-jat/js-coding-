b2dc1bf422c2e4d396ad8146434dc4b3c8ebefd3
tjatin1910-jat <tjatin1910@gmail.com>
2026-01-01 13:05:55 +0000

    new

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,3 +2,12 @@
 
 //alert(3+3) cant use this now as not in browser we using node js 
 
+let name = "jatin"
+let age = 18 
+let isloggedin  = false
+// number  = 2^53
+//boolean 
+// string
+// null = stand alone 
+// undefined =
+// symbol 
