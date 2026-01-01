2f2edf733ed73311a75232122a0de05913a891fa
tjatin1910-jat <tjatin1910@gmail.com>
2026-01-01 13:40:20 +0000

    new

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -24,6 +24,34 @@ let booleanisloggedin = Boolean(islogeedin)
 let somenumber = true
 let someletter = String(somenumber)
 
-console.log (someletter)
-console.log(typeof someletter)
+//console.log (someletter)
+//console.log(typeof someletter)
 
+// *********************************OPERATIONS********************//
+
+
+let value = 3
+let negvalue = -value
+// console.log (negvalue)
+// console.log(2+2)
+// console.table([2*2,2-2,2/2,2%3])
+
+let str1 = "hellow"
+let str2 = "hi "
+let str3 = str1 +str2
+// console.log(str3)
+
+// console.log ("1"+2)
+// console.log(1 + "2")
+// console.log("1"+2+2)
+// console.log(1+2+"2") // sting first then string or num first then arithmetic type 
+// console.log(+true) // increment herre so given 1 
+// console.log(+"") // increment case
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++; //prefix and postfix  js and mdn 
+console.log(gamecounter)
