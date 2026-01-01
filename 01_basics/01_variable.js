ae1c10e2065f0f9e1bf742739536f6e8f72c274d
tjatin1910-jat <tjatin1910@gmail.com>
2026-01-01 12:13:57 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "tjtain1910@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ludhiana"
+
+// accountId = 2 not aLLOWED 
+accountEmail = "DHV@GDVHW.COM"
+accountPassword = "324ND"
+accountCity = "cityjaipur "
+let accountstate ;
+
+/*
+prefer not to use the var 
+because of the issuein the block scopee and functional scope 
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,4 @@
+"use strict"; //treat all the js code as newer version 
+
+//alert(3+3) cant use this now as not in browser we using node js 
+
