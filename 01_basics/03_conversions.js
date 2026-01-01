1430e36e1704f3f142dc12fb916684202f41211c
tjatin1910-jat <tjatin1910@gmail.com>
2026-01-01 13:25:37 +0000

    conversion

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,29 @@
+let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber =  Number(score )
+
+// console.log(valueInNumber)
+
+
+// "33" = 33
+// '33abc' = NaN 
+// TRUE =1 AND FALSE = 0 
+
+let islogeedin = "jatin"
+let booleanisloggedin = Boolean(islogeedin)
+//console .log(booleanisloggedin)
+//console.log(typeof booleanisloggedin)
+
+// 1= true
+// "" = false 
+// "jatin" = true 
+ 
+let somenumber = true
+let someletter = String(somenumber)
+
+console.log (someletter)
+console.log(typeof someletter)
+
