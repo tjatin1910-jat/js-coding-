64a11022022c092876f80cdf4af1e1cc0147e4d1
tjatin1910-jat <tjatin1910@gmail.com>
2026-01-02 14:11:54 +0000

    DATE AND TIME ZONE

---
+++ 01_basics/07_date.js
@@ -0,0 +1,26 @@
+let mydate = new Date()
+console.log(mydate)
+console.log(mydate.toString());  
+console.log(mydate.toISOString());
+console.log(mydate.toDateString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleTimeString());
+console.log(typeof mydate); // object
+
+
+// let mydatecreated = new Date(2023,0,23) // in js months start from the 0 
+let mynewdate =  new Date("2023-01-14")
+// console.log(mynewdate.toString());
+
+
+let mytimestampt = Date.now()
+// console.log(mytimestampt);
+// console.log(mynewdate.getTime())
+// console.log(mynewdate)
+
+
+console.log();
+
+
